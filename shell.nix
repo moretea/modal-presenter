64e58c25cf1dda036a9dae2777b57b95c2fc1821
Maarten Hoogendoorn <maarten@moretea.nl>
2019-03-10 20:47:15 +0100

    rename to modal-presenter

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with (import <unstable> {});
 with pkgs;
 mkShell {
-  name = "cn-game-dev";
+  name = "modal-presenter-dev";
   buildInputs = [ rustc cargo SDL2 SDL2_image SDL2_ttf binutils xdo xdotool];
 }
